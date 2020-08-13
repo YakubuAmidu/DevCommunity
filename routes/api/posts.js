aff4b3b0b92f4e96486f125b50e7fc4cfebb097f
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-13 03:55:58 -0700

    Commiting changes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route GET api/post
+// @route GET api/posts
 // @desc  Test route
 // @access Public
-router.get('/', (req, res) => res.send('Post route'));
+router.get('/', (req, res) => res.send('Posts route'));
 
 module.exports = router;
