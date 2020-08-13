87d5fe155f7e4a2e43063fa8c45160a9ab25c62e
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-13 14:57:02 -0700

    Commiting changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -27,12 +29,27 @@ if (user) {
   res.status(400).json({ errors: [{ msg: "User already exist" }]});
 }
 
-  // Users gravatar
+const avatar = gravatar.url(email, {
+  s: '200',
+  r: 'pg',
+  d: 'mm'
+});
 
-  // Encrypt password
+user = new User({
+  name,
+  email,
+  avatar,
+  password
+});
+
+const salt = await bcrypt.genSalt(10);
+
+user.password = await bcrypt.hash(password, salt);
+
+await user.save();
 
   // return jsonwebtoken
-  res.send('User route')
+  res.send('User registered');
 } catch(err) {
 console.error(err.message);
 res.status(500).send('Sever error');
