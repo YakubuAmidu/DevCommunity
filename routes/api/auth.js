f1d9d1e52fa4852033378018420a003d454ec736
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-14 03:27:52 -0700

    Commiting changes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,7 +12,8 @@ router.get('/', auth, async (req, res) => {
  const user = await User.findById(req.user.id).select('-password');
  res.json(user);
   } catch(err) {
-
+console.error(err.message);
+res.status(500).send('Server Error');
   }
 });
 
