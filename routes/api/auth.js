a1f40c7c053d2467357d706eaec4b67cd7ce3590
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-15 00:45:06 -0700

    Commiting changes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -40,10 +40,14 @@ try {
 let user = await User.findOne({ email });
 
 if (user) {
-  return res.status(400).json({ errors: [{ msg: "Invalid credentials" }]});
+  return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }]});
 }
 
 const isMatch = await bcrypt.compare(password, user.password);
+
+if (!isMatch) {
+  return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }]});
+}
 
 const payload = {
   user: {
