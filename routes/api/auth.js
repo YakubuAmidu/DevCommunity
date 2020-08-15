84004285df2a83535700fb0e1e692bed901d2aa9
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-15 00:35:58 -0700

    Commiting changes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt= require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
 // @route GET api/auth
-// @desc  Test route
+// @desc  Authenticate user & get token
 // @access Public
 router.get('/', auth, async (req, res) => {
   try {
@@ -15,6 +19,48 @@ router.get('/', auth, async (req, res) => {
 console.error(err.message);
 res.status(500).send('Server Error');
   }
+});
+
+// @route POST api/users
+// @desc  Register user
+// @access Public
+router.post('/', [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Pasword is required').exists()
+],  async (req, res) =>{
+ const errors = validationResult(req);
+if(!errors.isEmpty()) {
+  return res.status(400).json({ errors: errors.array() });
+}
+
+const { email, password } = req.body;
+
+try {
+
+let user = await User.findOne({ email });
+
+if (user) {
+  return res.status(400).json({ errors: [{ msg: "Invalid credentials" }]});
+}
+
+const isMatch = await bcrypt.compare(password, user.password);
+
+const payload = {
+  user: {
+    id: user.id
+  }
+};
+
+jwt.sign(payload, config.get('jwtSecret'),
+{ expiresIn: 360000 }, (err, token) => {
+ if(err) throw err;
+ res.json({ token });
+});
+
+} catch(err) {
+console.error(err.message);
+res.status(500).send('Sever error');
+}
 });
 
 module.exports = router;
