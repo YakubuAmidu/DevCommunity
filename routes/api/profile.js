bf426ae8a11469da3c37bef3c8a14dc273cc1ea5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-16 01:36:33 -0700

    Commiting changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,8 +104,24 @@ router.get('/', async (req, res) => {
  } catch(err) {
    console.error(err.message);
    res.status(500).send('Server Error');
-
  }
 });
+
+// @route GET api/profile/user/:user_id
+// @desc  Get profile by user ID
+// @access Public
+router.get('/user/:user_Id', async (req, res) => {
+ try {
+ const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
+
+ res.json(profile);
+ } catch(err) {
+   console.error(err.message);
+   res.status(500).send('Server Error');
+ }
+});
+
 
 module.exports = router;
