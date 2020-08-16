a7e508a7288012cd59ddee7f93fece5cfc9bcce7
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-16 10:54:16 -0700

    Commiting changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,5 +144,20 @@ await User.findOneAndRemove({ _id: req.user.id });
  }
 });
 
+// @route PUT api/profile/experience
+// @desc  Add profile experience
+// @access Private
+
+router.put('/experience', [auth, [
+  check('title', 'Title is required').not().isEmpty(),
+  check('company', 'Company is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+const errors = validationResult(req);
+if (!errors.isEmpty()) {
+  return res.status(400).json({ errors: errors.array() });
+}
+});
+
 
 module.exports = router;
