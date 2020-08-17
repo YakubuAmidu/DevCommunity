3ece5079fac9855bf22bafc9c278ccfb5b3d1182
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-17 02:54:05 -0700

    Commiting changes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -17,6 +17,24 @@ router.post('/', [ auth, [
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
+try {
+  const user = await User.fintById(req.user.id).select('-password');
+
+  const newPost = {
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id
+};
+
+const post = await newPost.save();
+
+res.json(post);
+} catch(err) {
+  console.error(err.message);
+  res.status(500).send('Server Error');
+}
 });
 
 module.exports = router;
