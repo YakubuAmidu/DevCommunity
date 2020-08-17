1afc5341f323ffd853aef4779b5eeaa5aca1da69
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-17 11:50:24 -0700

    Commiting changes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -50,4 +50,22 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// @route GET api/posts/:id
+// @desc  Get all post by ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    return res.status(404).json({ msg: 'Post not found' });
+  }
+  res.json(post);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
