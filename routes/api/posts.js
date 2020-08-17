a765a3eead70d8fe2b25adc13ea625282868e215
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-17 02:38:12 -0700

    Commiting changes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 // @route POST api/posts
 // @desc  Create  a post
 // @access Private
-router.post('/', (req, res) => res.send('Posts route'));
+router.post('/', [ auth, [
+  check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+});
 
 module.exports = router;
