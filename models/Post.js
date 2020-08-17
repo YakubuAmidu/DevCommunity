c00dc2fbdd06f38547f1d09c41abbc7b28bec91f
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-17 02:16:44 -0700

    Commiting changes

--- models/Post.js
+++ models/Post.js
@@ -6,5 +6,22 @@ const PostSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'users'
   },
-  
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ]
 });
