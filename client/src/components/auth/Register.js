af59cf9ccd2c0f127e3732d5f2661fa0efd62477
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-18 13:50:40 -0700

    Commiting changes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
@@ -9,10 +21,24 @@ const Register = () => {
       </p>
       <form className='form' action='create-profile.html'>
         <div className='form-group'>
-          <input type='text' placeholder='Name' name='name' required />
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className='form-group'>
-          <input type='email' placeholder='Email Address' name='email' />
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -23,6 +49,8 @@ const Register = () => {
             type='password'
             placeholder='Password'
             name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
             minLength='6'
           />
         </div>
@@ -32,6 +60,8 @@ const Register = () => {
             placeholder='Confirm Password'
             name='password2'
             minLength='6'
+            value={password2}
+            onChange={(e) => onChange(e)}
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
