2fcb0cb921f56095e0f3f8f2693064074697927b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-21 00:03:24 -0700

    Commiting changes

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@
 import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 5000 ) => (dispatch) => {
   //   const id = uuid.v4();
   const id = uuid;
   dispatch({
@@ -10,5 +10,5 @@ export const setAlert = (msg, alertType) => (dispatch) => {
     payload: { msg: alertType, id }
   });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
