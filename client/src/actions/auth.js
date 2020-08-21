f7659b126edb91194f9ca9ba9dbef86c8f132b3f
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-21 00:44:40 -0700

    Commiting changes

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { setAlert } from './alert';
+
 import {
 REGISTER_SUCCESS,
 REGISTER_FAIL
@@ -21,6 +23,12 @@ export const register = ({ name, email, password }) => async dispatch => {
       payload: res.data
     });
   } catch(err) {
+   const errors = err.response.data.errors;
+
+   if (errors) {
+     errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+   }
+
     dispatch({
       type: REGISTER_FAIL
     });
