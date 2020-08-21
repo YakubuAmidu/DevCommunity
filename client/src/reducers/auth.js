22d62184f0becad2519821ed2b4e931a87d6966a
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-21 00:17:49 -0700

    Commiting changes

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -8,3 +8,18 @@ const initialState = {
   isAuthenticated: null,
   user: null
 }
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch(type) {
+    case REGISTER_SUCCESS:
+     localStorage.setItem('token', payload.token);
+     return {
+       ...state,
+       ...payload,
+       isAuthenticated: true,
+       loading: false
+     }
+  }
+}
