d3798e7751f2489fd7246c382bccd2f8ea67942b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-22 02:14:42 -0700

    Commiting changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,20 +4,36 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+ const authLinks = (
+   <ul>
+     <li><a onClick={logout} href="#!">Developers</a></li>
+   </ul>
+ );
+
+ const guestLinks = (
+   <ul>
+     <li><a href="#!"><i className="fas fa-sign-out-alt"></i>{' '}
+     <span className="hide-sm">Logout</span></a></li>
+     <li><Link to='/register'>Register</Link></li>
+     <li><Link to='/login'>Login</Link></li>
+   </ul>
+ );
+
   return (
     <nav className="navbar bg-dark">
       <h1>
         <Link to='/'><i className="fas fa-code"></i> DevCommunity</Link>
       </h1>
-      <ul>
-        <li><a href="!#">Developers</a></li>
-        <li><Link to='/register'>Register</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-      </ul>
+
     </nav>
   );
 };
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
 
 const mapStateToProps = state => ({
   auth: state.auth
