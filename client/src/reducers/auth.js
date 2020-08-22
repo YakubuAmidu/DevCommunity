a14c5129e3d5a9508683a463456850383266665e
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-22 00:56:02 -0700

    Commiting changes

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,10 @@
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL, USER_LOADED, AUTH_ERROR
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
