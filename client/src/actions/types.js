6e49fd0beb4d6f098edd800511887612254451ce
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 01:54:48 -0700

    Commiting changes

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,4 +8,4 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const GET_PROFILE = 'GET_PROFILE';
-export const PROFIL_ERROR = 'PROFILE_ERROR';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
