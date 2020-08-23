e66a62756ca98685f50a0b477a6f2d33f572cdb3
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 01:06:42 -0700

    Commiting changes

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -3,9 +3,22 @@ import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const PrivateRoute = ({ component: Component, ...res }) => {
-  return <div></div>;
-};
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      !isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired,
