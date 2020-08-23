68b1d17187051234dbcd1ceea67e01ded556f87e
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 02:03:30 -0700

    Commiting changes

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -15,6 +15,13 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false,
+      };
+
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
         loading: false,
       };
   }
