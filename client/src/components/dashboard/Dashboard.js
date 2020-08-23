b8c17b182b0333683362b8f35bfd3e937c57e796
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 02:18:34 -0700

    Commiting changes

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = (props) => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return <div>Dashboard</div>;
 };
 
