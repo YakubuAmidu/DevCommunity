5dcab4fbff9b5ffc85f271ee043059ed72bca013
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 03:03:39 -0700

    Commiting changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,39 +5,58 @@ import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
- const authLinks = (
-   <ul>
-     <li><a onClick={logout} href="#!">
-     <i className="fas fa-sign-out-alt" />{' '}
-     <span className="hide-sm">Logout</span></a></li>
-   </ul>
- );
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
 
- const guestLinks = (
-   <ul>
-     <li><a href="#!">Developers</a></li>
-     <li><Link to='/register'>Register</Link></li>
-     <li><Link to='/login'>Login</Link></li>
-   </ul>
- );
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='#!'>Developers</a>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
 
   return (
-    <nav className="navbar bg-dark">
+    <nav className='navbar bg-dark'>
       <h1>
-        <Link to='/'><i className="fas fa-code" /> DevCommunity</Link>
+        <Link to='/'>
+          <i className='fas fa-code' /> DevCommunity
+        </Link>
       </h1>
-   { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-}
+  auth: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(Navbar);
