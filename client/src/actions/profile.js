b36fd1b52f85e84fed6b134b4975a90677ee80cf
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 01:58:01 -0700

    Commiting changes

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -12,5 +12,10 @@ export const getCurrentProfile = () => async (dispatch) => {
       type: GET_PROFILE,
       payload: res.data,
     });
-  } catch (err) {}
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
 };
