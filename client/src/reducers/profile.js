69c0090c5de854a3dc923f62db7bcf34dd7bfdce
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-23 01:46:01 -0700

    Commiting changes

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,13 @@
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  error: {},
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+  }
+}
