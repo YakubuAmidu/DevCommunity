9756d3b6923d3c108ae7ba6ca403897f9970bcc5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-24 03:41:00 -0700

    Commiting changes

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -56,3 +56,36 @@ export const createProfile = (formData, history, edit = false) => async (
     });
   }
 };
+
+// Add Experience
+export const addExperience = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put('/api/profile/experience', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Experience added', 'success'));
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
