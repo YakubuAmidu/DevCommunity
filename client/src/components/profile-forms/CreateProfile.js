7de994894a35081b589655020a753a32e1cdce57
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-24 00:13:08 -0700

    Commiting changes

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = (props) => {
+  const [formData, setFormDate] = useState({
+    company: '',
+    website: ' ',
+    location: ' ',
+    status: ' ',
+    skills: ' ',
+    githubusername: ' ',
+    bio: ' ',
+    twitter: ' ',
+    facebook: ' ',
+    linkedin: ' ',
+    youtube: ' ',
+    instagram: '',
+  });
+
+  return <div></div>;
+};
+
+CreateProfile.prototypes = {};
+
+export default CreateProfile;
