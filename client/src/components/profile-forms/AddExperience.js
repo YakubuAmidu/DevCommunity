8dbb22fd9a48ce69d9e996147dc53de2fa680e65
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-25 11:52:25 -0700

    Commiting changes

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions/profile';
 
 const AddExperience = () => {
   return <div>addExperience</div>;
