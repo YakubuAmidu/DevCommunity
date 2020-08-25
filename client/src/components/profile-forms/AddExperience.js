ca1a7b8d48bab5d16ae1fa2f68ea9e852d037a6b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-25 12:15:55 -0700

    Commiting changes

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,13 +1,102 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExperience } from '../../actions/profile';
 
 const AddExperience = () => {
-  return <div>addExperience</div>;
+  const [formData, setFormData] = useState({
+    company: '',
+    title: '',
+    location: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+  });
+
+  const [toDateDisabled, toggleDisabled] = useState(false);
+
+  const { company, title, location, to, current, from, description } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Fragment>
+      <h1 class='large text-primary'>Add An Experience</h1>
+      <p class='lead'>
+        <i class='fas fa-code-branch'></i> Add any developer/programming
+        positions that you have had in the past
+      </p>
+      <small>* = required field</small>
+      <form class='form'>
+        <div class='form-group'>
+          <input
+            type='text'
+            placeholder='* Job Title'
+            name='title'
+            value={title}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div class='form-group'>
+          <input
+            type='text'
+            placeholder='* Company'
+            name='company'
+            value={company}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div class='form-group'>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div class='form-group'>
+          <h4>From Date</h4>
+          <input
+            type='date'
+            name='from'
+            value={from}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div class='form-group'>
+          <p>
+            <input type='checkbox' name='current' value='' /> Current Job
+          </p>
+        </div>
+        <div class='form-group'>
+          <h4>To Date</h4>
+          <input type='date' name='to' />
+        </div>
+        <div class='form-group'>
+          <textarea
+            name='description'
+            cols='30'
+            rows='5'
+            placeholder='Job Description'
+          ></textarea>
+        </div>
+        <input type='submit' class='btn btn-primary my-1' />
+        <a class='btn btn-light my-1' href='dashboard.html'>
+          Go Back
+        </a>
+      </form>
+    </Fragment>
+  );
 };
 
-AddExperience.propTypes = {};
+AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
+};
 
-export default AddExperience;
+export default connect(null, { addExperience })(AddExperience);
