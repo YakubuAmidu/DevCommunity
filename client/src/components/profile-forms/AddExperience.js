1f1a2db88585f7fcb4e9c29d155701fe5a419d5b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-25 11:49:08 -0700

    Commiting changes

---
+++ client/src/components/profile-forms/AddExperience.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AddExperience = () => {
+  return <div>addExperience</div>;
+};
+
+AddExperience.propTypes = {};
+
+export default AddExperience;
