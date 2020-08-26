c5fdb8c5aaaec28f4b1d8a3f6399ef213a555e28
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-26 02:54:52 -0700

    Commiting changes

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { deleteEducation } from '../../actions/profile';
 
 const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
