11b27a1c8d094102b82684db387d569126f51755
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-26 02:00:12 -0700

    Commiting changes

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,47 +5,49 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  ACCOUNT_DELETED,
 } from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
-  user: null
-}
+  user: null,
+};
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
-  switch(type) {
+  switch (type) {
     case USER_LOADED:
-    return {
-      ...state,
-      isAuthenticated: true,
-      loading: false,
-      user: payload
-    };
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-     localStorage.setItem('token', payload.token);
-     return {
-       ...state,
-       ...payload,
-       isAuthenticated: true,
-       loading: false
-     };
-     case REGISTER_FAIL:
-     case AUTH_ERROR:
-     case LOGIN_FAIL:
-     case LOGOUT:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
+    case ACCOUNT_DELETED:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
       };
-      default:
+    default:
       return state;
   }
 }
