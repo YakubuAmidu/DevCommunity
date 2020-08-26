bfcd870cc1a4f54248d4fc75e3aa0f1a72957262
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-26 03:40:31 -0700

    Commiting changes

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -46,6 +46,23 @@ export const getProfiles = () => async (dispatch) => {
   }
 };
 
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.responst.status },
+    });
+  }
+};
+
 // Create or update a profile
 export const createProfile = (formData, history, edit = false) => async (
   dispatch
