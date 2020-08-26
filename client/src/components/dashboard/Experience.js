cfc9e4dad04b07b5a1a01bdc29da05c0b68c41f8
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-25 23:52:02 -0700

    Commiting changes

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
 
 const Experience = ({ experience }) => {
   return (
-    <div>Experience</div>
+    <Fragment>
+      <h2 className='my-2'>Experience Credentials</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th className='hide-sm'>Title</th>
+            <th className='hide-sm'>Years</th>
+            <th />
+          </tr>
+        </thead>
+      </table>
+    </Fragment>
   );
-}
+};
 
-Experience.propTypes = {
-
-}
+Experience.propTypes = {};
 
 export default Experience;
