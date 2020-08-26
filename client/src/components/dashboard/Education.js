9d4e3ad219e6a04e698fa6ffb02741728a2a4947
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-26 01:21:39 -0700

    Commiting changes

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -3,21 +3,26 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
-const Education = ({ education }) => {
+const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
-    <tr key={edu_id}>
+    <tr key={edu._id}>
       <td>{edu.school}</td>
       <td className='hide-sm'>{edu.degree}</td>
       <td>
-        <Moment format='YYYY/MM/DD'>{edu.from}</Moment> -{' '}
+        <Moment format='YYYY/MM/DD'>{moment.utc(edu.from)}</Moment> -{' '}
         {edu.to === null ? (
-          ' NOW'
+          ' Now'
         ) : (
-          <Moment format='YYYY/MM/DD'>{edu.to}</Moment>
+          <Moment format='YYYY/MM/DD'>{moment.utc(edu.to)}</Moment>
         )}
       </td>
       <td>
-        <button className='btn btn-danger'>Delete</button>
+        <button
+          onClick={() => deleteEducation(edu._id)}
+          className='btn btn-danger'
+        >
+          Delete
+        </button>
       </td>
     </tr>
   ));
