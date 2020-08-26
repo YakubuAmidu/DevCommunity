cb1db1eebc56b5e672afe1a3eb0a9e3757cd56b6
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-26 07:01:19 -0700

    Commting changes

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -11,9 +11,33 @@ const ProfileItem = ({
     skills,
   },
 }) => {
-  return <div>test</div>;
+  return (
+    <div className='profile bg-light'>
+      <img src={avatar} alt='' className='round-img' />
+      <div>
+        <h2>{name}</h2>
+        <p>
+          {status} {company && <span> at {company}</span>}
+        </p>
+        <p className='my-1'>{location && <span>{location}</span>}</p>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+          View Profile
+        </Link>
+      </div>
+      <ul>
+        {skills.slice(0, 4).map((skill, index) => (
+          <li key={index} className='text-primary'>
+            <i className='fas fa-check'></i>
+            {skills}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-ProfileItem.propTypes = {};
+ProfileItem.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
 
 export default ProfileItem;
