3b395b46af2cb253ead9289f9d71db699fa12721
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-25 23:45:17 -0700

    Commiting changes

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Experience = ({ experience }) => {
+  return (
+    <div>Experience</div>
+  );
+}
+
+Experience.propTypes = {
+
+}
+
+export default Experience;
