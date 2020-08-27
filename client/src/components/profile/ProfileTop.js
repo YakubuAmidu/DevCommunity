0e3be782479e4a18a57e2ed56d165fa23c09a1f1
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 02:24:19 -0700

    Commiting changes

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -14,28 +14,42 @@ const ProfileTop = ({
   return (
     <div class='profile-top bg-primary p-2'>
       <img class='round-img my-1' src={avatar} alt='' />
-      <h1 class='large'>John Doe</h1>
-      <p class='lead'>Developer at Microsoft</p>
-      <p>Seattle, WA</p>
+      <h1 class='large'>{name}</h1>
+      <p class='lead'>
+        {status} {company && <span>at {company}</span>}
+      </p>
+      <p>{location && <span>{location}</span>}</p>
       <div class='icons my-1'>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fas fa-globe fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-twitter fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-facebook fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-linkedin fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-youtube fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-instagram fa-2x'></i>
-        </a>
+        {website && (
+          <a href={website} target='_blank' rel='noopener noreferrer'>
+            <i class='fas fa-globe fa-2x'></i>
+          </a>
+        )}
+        {social && social.twitter && (
+          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-twitter fa-2x'></i>
+          </a>
+        )}
+        {social && social.facebook && (
+          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-facebook fa-2x'></i>
+          </a>
+        )}
+        {social && social.linkedin && (
+          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-linkedin fa-2x'></i>
+          </a>
+        )}
+        {social && social.youtube && (
+          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-youtube fa-2x'></i>
+          </a>
+        )}
+        {social && social.instagram && (
+          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-instagram fa-2x'></i>
+          </a>
+        )}
       </div>
     </div>
   );
