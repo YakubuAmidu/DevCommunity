da930834dfb89eb96220afc2ffa5835aa5ac1d27
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 02:27:37 -0700

    Commiting changes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById, getProfiles } from '../../actions/profile';
 
 const Profile = ({
@@ -35,6 +36,7 @@ const Profile = ({
 
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}
