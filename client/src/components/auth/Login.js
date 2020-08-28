33d5114328a78127b423b8a1cf71cf29aff4dd10
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-28 02:02:22 -0700

    Commiting changes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -29,7 +29,7 @@ const Login = ({ login, isAuthenticated }) => {
     <Fragment>
       <h1 className='large text-primary'>Sign In</h1>
       <p className='lead'>
-        <i className='fas fa-user'></i> Sign into Your Account
+        <i className='fas fa-user' /> Sign into Your Account
       </p>
       <form className='form' onSubmit={(e) => onSubmit(e)}>
         <input
