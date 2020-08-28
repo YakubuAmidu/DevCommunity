671d9ad847b47c949c61a1ccafc0f33ef1179a08
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 20:33:47 -0700

    Commiting changes

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPosts } from '../../actions/post';
+
+const Posts = (props) => {
+  return <div></div>;
+};
+
+Posts.propTypes = {};
+
+export default Posts;
