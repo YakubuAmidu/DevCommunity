59d48f8c3d67a9a5aa3b6161d797d4099a2d3942
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 22:37:37 -0700

    Commiting changes

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -9,6 +9,47 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.statusText,
+      },
+    });
+  }
+};
+
+
+// Add like
+export const addLike = postId => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: { res.data }}
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.statusText,
+      },
+    });
+  }
+};
+
+// Remove like
+export const removeLike = postId => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: { res.data }}
     });
   } catch (err) {
     dispatch({
