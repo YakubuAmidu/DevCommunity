a46d8995ae56d138dd27901f53493ec3e36b5c54
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 23:26:25 -0700

    Commiting changes

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -23,13 +23,13 @@ export const getPosts = () => async (dispatch) => {
 
 
 // Add like
-export const addLike = postId => async (dispatch) => {
+export const addLike = id => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/like/${postId}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { id, likes: { res.data }}
+      payload: { id, likes:  res.data }
     });
   } catch (err) {
     dispatch({
@@ -43,13 +43,13 @@ export const addLike = postId => async (dispatch) => {
 };
 
 // Remove like
-export const removeLike = postId => async (dispatch) => {
+export const removeLike = id => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/unlike/${postId}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { id, likes: { res.data }}
+      payload: { id, likes: res.data }
     });
   } catch (err) {
     dispatch({
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }})=>
+const PostItem = ({ addLike, removeLike, auth, post: { _id, text, name, avatar, user, likes, comments, date }})=>
     <div class="post bg-white p-1 my-1">
           <div>
             <a href="profile.html">
@@ -23,13 +24,13 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
              <p class="post-date">
                 Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
-            <button type="button" class="btn btn-light">
+            <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
               <i class="fas fa-thumbs-up"></i> {'  '}
               <span>{likes.length > 0 && (
                 <span>{likes.length}</span>
               )}</span>
             </button>
-            <button type="button" class="btn btn-light">
+            <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
               <i class="fas fa-thumbs-down"></i>
             </button>
             <Link to={`/post/${_id}`} class="btn btn-primary">
@@ -57,4 +58,4 @@ const mapStateToProps = state => ({
 auth: state.auth
 })
 
-export default connect(mapStateToProps, { })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
