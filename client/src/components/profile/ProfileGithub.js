7ed8eefc4d974add27c32cad22f97ed22e89a6d4
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 19:52:12 -0700

    Commiting changes

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -18,11 +18,29 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
       ) : (
         repos.map((repo) => (
           <div key={repo._id} className='repo bg-white p-1 my-1'>
-            <h4>
-              <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
-                {repo.name}
-              </a>
-            </h4>
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className='badge badge-primary'>
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className='badge badge-dark'>
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className='badge badge-light'>Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
           </div>
         ))
       )}
