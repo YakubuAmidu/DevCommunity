937ad16c0b46f1479372ea7216272d3758ca2f58
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-28 00:52:59 -0700

    Commiting changes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,7 +18,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
