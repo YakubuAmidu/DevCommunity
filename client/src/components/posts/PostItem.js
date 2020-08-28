438e4588290ae84d4b2e3b82f9672ba7f5c3b243
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-27 21:43:25 -0700

    Commiting changes

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
+const PostItem = props=> {
+  return (
+    <div></div>
+  )
+}
+
+PostItem.propTypes = {
+
+}
+
+const mapStateToProps = state => ({
+
+})
+
+export default connect(null, { })(PostItem);
