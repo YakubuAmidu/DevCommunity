67170f64dd30089acd5d0ab10c0068e06bd10760
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-29 01:23:57 -0700

    Commiting changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
+const path = require(' ');
 
 const app = express();
 
-// Connect Database
+// connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -30,29 +30,34 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-
-
-
-
 // const express = require('express');
 // const connectDB = require('./config/db');
-//
+// const path = require('path');
+
 // const app = express();
-//
-// // connect Database
+
+// // Connect Database
 // connectDB();
-//
+
 // // Init Middleware
-// app.use(express.json({ extended: false }));
-//
-// app.get('/', (req, res) => res.send('API Running'));
-//
+// app.use(express.json());
+
 // // Define Routes
 // app.use('/api/users', require('./routes/api/users'));
 // app.use('/api/auth', require('./routes/api/auth'));
 // app.use('/api/profile', require('./routes/api/profile'));
 // app.use('/api/posts', require('./routes/api/posts'));
-//
+
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
 // const PORT = process.env.PORT || 5000;
-//
+
 // app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
