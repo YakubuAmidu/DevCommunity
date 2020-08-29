be884c5212e91f70bdb660a57070bdc944611efd
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-29 01:54:53 -0700

    Commiting changes

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -6,6 +6,7 @@ const NotFound = () => {
       <h1 className='x-large text-primary'>
         <i className='fas fa-exclamation-triangle'></i> Page not found
       </h1>
+      <p className='large'>Sorry this page does not exist</p>
     </Fragment>
   );
 };
